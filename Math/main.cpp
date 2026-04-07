43131a84df58c12978800b982f497435b2f11006
nashimoff <nurlan.ashimoff@gmail.com>
2026-04-08 00:34:23 +0600

    v

---
+++ Math/main.cpp
@@ -0,0 +1,23 @@
+//Section 11
+//Math examples
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+    double num {};
+
+    cout << "Enter a number (double): ";
+    cin >> num;
+
+    cout << "The sqrt of " << num << " is: " << sqrt(num) << endl;
+    cout << "The cubed root of " << num << " is: " << cbrt(num) << endl;
+
+    cout << "The sine of " << num << " is: " << sin(num) << endl;
+    cout << "The cosine of " << num << "is: " << cos(num) << endl;
+
+    cout << "The ceil of " << num << "is: " << ceil(num) << endl;
+}
