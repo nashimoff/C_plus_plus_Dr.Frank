17d5b6772f2b3a0faa5c7f1397cb7eff92c352b6
nashimoff <nurlan.ashimoff@gmail.com>
2026-04-08 00:15:52 +0600

    v

--- ChallengeSolution/main.cpp
+++ ChallengeSolution/main.cpp
@@ -36,8 +36,8 @@ using namespace std;
 
 int main() {
 
- string alphabet {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
- string key  {"XZNLWEBGJHQDYVTKFUOMPCIASRxznlwebgjhqdyvtkfuompciasr"};
+ string alphabet {"[ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+ string key  {" [XZNLWEBGJHQDYVTKFUOMPCIASRxznlwebgjhqdyvtkfuompciasr"};
 
  string secret_message {};
  cout << "Enter your secret message : ";
@@ -58,6 +58,22 @@ int main() {
  }
 
  cout << "\nEncrypted message: " << encrypted_message << endl;
+
+ string decrypted_message {};
+ cout << "\nDecrypting message..." << endl;
+
+ for (char c: encrypted_message) {
+  size_t position = key.find(c);
+  if (position != string::npos) {
+   char new_char { alphabet.at(position)};
+   decrypted_message += new_char;
+  } else {
+   decrypted_message += c;
+  }
+ }
+
+ cout << "\nDecrypted message: " << decrypted_message << endl;
+
 
  cout << endl;
  return 0;
