c1e5a89c7317c25e3772a62a0f83aa66242aa198
Nurlan Ashimov <nashimoff@gmail.com>
2026-04-21 23:09:44 +0600

    a

--- Section11/DefaultArguments/main.cpp
+++ Section11/DefaultArguments/main.cpp
@@ -2,13 +2,20 @@
 // Default Arguments
 #include <iostream>
 #include <iomanip>
+#include <string>
 
 using namespace std;
 
 double calc_cost(double base_cost = 100.0, double tax_rate = 0.06, double shipping = 3.50);
 
+void greeting(string name, string prefix = "Mr,", string suffix = "");
+
 double calc_cost(double base_cost, double tax_rate, double shipping) {
     return base_cost += (base_cost * tax_rate) + shipping;
+}
+
+void greeting(string name, string prefix, string suffix) {
+    cout << "Hello " << prefix + " " + name + " " + suffix << endl;
 }
 
 int main() {
@@ -23,6 +30,15 @@ int main() {
 
     cost = calc_cost(200.0);  // will use default tax and shipping 200 + 12 + 3.50
     cout << "Cost is: " << cost << endl;          // 215.50
+
+    cost = calc_cost();
+    cout << "Cost is: " << cost << endl; // 100 + 6 + 3.50
+
+    greeting("Glenn Jones", "Dr.", "M.D.");
+    greeting("James Rogers", "Professor", "Ph.D.");
+    greeting("Frank Miller", "Dr.");
+    greeting("William Smith");
+    greeting("Mary Howard", "Mrs.", "Ph.D.");
 
     cout << endl;
     return 0;
