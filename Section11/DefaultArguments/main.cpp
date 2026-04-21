66d90b6fe1fc385a73c646924023b1a6bf436b05
nashimoff <159990431+nashimoff@users.noreply.github.com>
2026-04-21 11:50:31 +0600

    a

--- Section11/DefaultArguments/main.cpp
+++ Section11/DefaultArguments/main.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-double calc_cost(double base_cost, double tax_rate = 0.06, double shipping = 3.50);
+double calc_cost(double base_cost = 100.0, double tax_rate = 0.06, double shipping = 3.50);
 
 double calc_cost(double base_cost, double tax_rate, double shipping) {
     return base_cost += (base_cost * tax_rate) + shipping;
@@ -13,11 +13,18 @@ double calc_cost(double base_cost, double tax_rate, double shipping) {
 
 int main() {
     double cost{0};
-    cost = cal_cost(100.0, 0.08, 4.25);    // will use no defaults 100+ 8 + + 4.25
+    cost = calc_cost(100.0, 0.08, 4.25);    // will use no defaults 100+ 8 + + 4.25
 
     cout << fixed << setprecision(2);
     cout << "Cost is: " << cost << endl;   // 112.25
 
     cost = calc_cost(100.0, 0.08);  // will use default shipping 100 + 8 + 3.50
-    cout << "Cost is: " << endll;          // 111.50
+    cout << "Cost is: " << cost << endl;          // 111.50
+
+    cost = calc_cost(200.0);  // will use default tax and shipping 200 + 12 + 3.50
+    cout << "Cost is: " << cost << endl;          // 215.50
+
+    cout << endl;
+    return 0;
+
 }
