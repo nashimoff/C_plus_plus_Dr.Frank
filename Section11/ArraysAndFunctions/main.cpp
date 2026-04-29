4904a85da81773664735d80145908c33331a3abb
nashimoff <159990431+nashimoff@users.noreply.github.com>
2026-04-29 16:06:49 +0600

    a

---
+++ Section11/ArraysAndFunctions/main.cpp
@@ -0,0 +1,24 @@
+// Section 11
+// Arrays and functions
+#include <iostream>
+
+using namespace std;
+
+void print_array(int arr[], size_t size);
+void set_array(int arr[], size_t size, int value);
+
+void print_array(int arr[], size_t size) { // const
+    for (size_t i{0}; i < size; ++i)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+// set each array element to value
+void set_array(int arr[], size_t size, int value) {
+    for (size_t i{0}; i < size; ++i)
+        arr[i] = value;
+}
+
+int main() {
+
+}
