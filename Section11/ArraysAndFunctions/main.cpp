e6160305f612bccdf04a8283b51882f03213b636
nashimoff <159990431+nashimoff@users.noreply.github.com>
2026-04-29 16:16:45 +0600

    a

--- Section11/ArraysAndFunctions/main.cpp
+++ Section11/ArraysAndFunctions/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 void print_array(int arr[], size_t size);
 void set_array(int arr[], size_t size, int value);
 
-void print_array(int arr[], size_t size) { // const
+void print_array(int arr[], size_t size) { // const //
     for (size_t i{0}; i < size; ++i)
         cout << arr[i] << " ";
     cout << endl;
